6fe63ced2983eb7ffd58c12b2de15c0dbb456730
Erhan Ozturk <oztrkerhan@gmail.com>
2026-03-31 23:38:44 +0100

    feat: setup project structure and implement Appliance module

---
+++ appliance.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Priority(Enum):
+    LOW = 1
+    HIGH = 2
+    CRITICAL = 3
+
+class Appliance:
+    def __init__(self, name: str, power_draw: float, priority: Priority):
+        self.name = name
+        self.power_draw = power_draw
+        self.priority = priority
+        self.is_on = False
+
+    def turn_on(self):
+        self.turn_on = True
+
+    def turn_off(self):
+        self.turn_off = False
+        
